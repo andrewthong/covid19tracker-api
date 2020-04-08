bafda4549efa18e9e1609a94d108f5059d99dab6
Andrew <andrew.thong@digitalistgroup.com>
2020-04-07 19:18:10 -0700

    table migrations

---
+++ database/migrations/2020_04_08_021031_create_cases_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->id();
+            $table->string('province');
+            $table->string('city', 100);
+            $table->string('age', 50);
+            $table->string('travel_history');
+            $table->string('confirmed_presumptive');
+            $table->string('source');
+            $table->datetime('date', 0);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cases');
+    }
+}
