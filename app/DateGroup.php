b2cb24d81a5fb96b2faeadc5e0d7a601c0426996
Andrew <andrew.thong@digitalistgroup.com>
2020-04-07 23:10:01 -0700

    setting up dategroup model

---
+++ app/DateGroup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DateGroup extends Model
+{
+    protected $table = 'date_group';
+
+    protected $guarded = [
+        'id',
+    ];
+}
---
+++ database/migrations/2020_04_08_060620_create_date_group_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDateGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('date_group', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('group');
+            $table->integer('cases');
+            $table->integer('cumulative_cases');
+            $table->integer('fatalities');
+            $table->integer('cumulative_fatalities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('date_group');
+    }
+}
