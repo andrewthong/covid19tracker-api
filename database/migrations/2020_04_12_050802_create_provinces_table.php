b3183377c8601f52a7916490dd5fefc5862d9b01
Andrew <andrew.thong@digitalistgroup.com>
2020-04-11 23:48:58 -0700

    provinces table tweaks
    unique code; basic api route

--- database/migrations/2020_04_12_050802_create_provinces_table.php
+++ database/migrations/2020_04_12_050802_create_provinces_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProvinceTable extends Migration
+class CreateProvincesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,14 +15,14 @@ class CreateProvinceTable extends Migration
     {
         Schema::create('provinces', function (Blueprint $table) {
             $table->id();
-            $table->string('code', 8);
+            $table->string('code', 8)->unique();
             $table->string('name');
             // reference where tracking information is retrieved from
             $table->string('data_source')->nullable();
             // additional attributes that may be useful
             $table->integer('population')->nullable();
             // leaving the unit of measurement out to allow for some flexibility
-            // want to use sq miles and billions, sure
+            // want to use sq miles with total area (land and water), and billions for gdp, go for it
             $table->float('area', 9, 2)->nullable();
             $table->integer('gdp')->nullable();
         });
