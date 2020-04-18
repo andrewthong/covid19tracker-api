1ed56adf7058f339e4096e2b01838ce235be9999
Andrew <andrew.thong@digitalistgroup.com>
2020-04-17 20:10:46 -0700

    API polish and tune-up
    - reports now support fill_dates and cumulative parameters
    - created Utility model to host internal functions
    - added fatalities controller

---
+++ app/Http/Controllers/FatalityController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Fatality;
+
+class FatalityController extends Controller
+{
+    /*
+    fatalities list
+     */
+    public function list(Request $request, $province = null) {
+
+        // province support
+        if( $request->province ) {
+            $province = $request->province;
+        }
+
+        // pagination
+        $per_page = 100;
+        if( is_int($request->per_page) ) {
+            $per_page = max( $request->per_page, 1000 );//limit
+        }
+
+        $order = 'DESC';
+        $order_by = 'id';
+        if( $request->order === 'ASC' ) {
+            $order = $request->order;
+        }
+
+        $cases = DB::table('fatalities')
+            ->when( $province, function($query) use ($province) {
+                // if a province is provided; otherwise all
+                return $query->where('province', $province);
+            })
+            ->orderBy('id', $order)
+            ->paginate($per_page);
+
+        return $cases;
+    }
+
+    /*
+    get specific fatality
+     */
+    public function get($id) {
+        return Fatality::find($id);
+    }
+}
---
+++ app/Utility.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\DB;
+
+use App\Common;
+use App\Province;
+
+class Utility
+{
+    /**
+     * utility function to convert provinces attribute in defined tables
+     * $to_code: if true, converts names to code where applicable
+     */
+    static function transformProvinces( $to_code = true ) {
+        // modular from-to
+        $vfrom = 'name';
+        $vto = 'code';
+        // swap them around (from code to names)
+        if( $to_code !== false ) {
+            list( $vto, $vfrom ) = array( $vto, $vfrom );
+        }
+        // get all provinces
+        $provinces = Province::all()->toArray();
+        $result = array();
+
+        // tables to comb through
+        $tables = [
+            'cases',
+            'fatalities',
+        ];
+    
+        foreach( $tables as $table ) {
+            foreach( $provinces as $province ) {
+                // run an update statement for each province
+                // Eloquent equivalent to
+                //   update `cases` set `province` = 'code_or_name' where `province` = 'name_or_code' 
+                $affected_rows = DB::table( $table )
+                    ->where( 'province', $province[$vfrom] )
+                    ->update( ['province' => $province[$vto]] );
+                $result[] = array(
+                    'from' => $province[$vfrom],
+                    'to' => $province[$vto],
+                    'affected_rows' => $affected_rows,
+                );
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * this utility function takes counts cases and fatalities to fill the reports
+     * needed for reporting unless reports also include cases and fatalities
+     * current tracking logs cases and fatalities on an individual level
+    */
+    static function transferToReports() {
+
+        $provinces = Province::all()->pluck('code');
+        
+        // count known provinces and records using province code
+        $provinces_in = implode( "','", $provinces->toArray() );
+        $where_stmt = "WHERE province IN ('{$provinces_in}')";
+
+        // query to count daily cases and fatalities from individual db
+        $records = DB::select("
+            SELECT
+                province,
+                day,
+                COUNT(c_id) as cases,
+                COUNT(f_id) as fatalities
+            FROM (
+                SELECT
+                    province,
+                    DATE(`date`) AS day,
+                    id AS c_id,
+                    null AS f_id
+                FROM 
+                    `cases`
+                UNION
+                SELECT
+                    province,
+                    DATE(`date`) AS day,
+                    null as c_id,
+                    id AS f_id
+                FROM
+                    `fatalities`
+            ) AS un
+            {$where_stmt}
+            GROUP BY
+                day,
+                province
+            ORDER BY
+                day
+        ");
+
+        $response = [];
+
+        foreach( $records as $record ) {
+            DB::table('reports')
+                ->updateOrInsert(
+                    [
+                        'date' => $record->day,
+                        'province' => $record->province
+                    ],
+                    [
+                        'date' => $record->day,
+                        'province' => $record->province,
+                        'cases' => $record->cases,
+                        'fatalities' => $record->fatalities,
+                    ]
+                );
+        }
+
+        return $response;
+        
+    }
+
+    /**
+     * create records of gaps between dates
+     * facilitates time series charting and cumulative calculation
+     */
+    static function dateFillReports() {
+        // only for registered provinces
+        $provinces = Province::all();
+        // get earliest and latest report dates
+
+        // loop through provinces
+            // loop through dates
+                // if missing, add null entry
+                // log  
+    }
+
+}
