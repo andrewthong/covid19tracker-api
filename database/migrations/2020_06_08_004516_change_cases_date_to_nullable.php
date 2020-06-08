c2c61855648bb2f6865a36138f320da24c40b19c
Andrew <contact@andrewthong.com>
2020-06-07 23:46:03 -0700

    initial migration

---
+++ database/migrations/2020_06_08_004516_change_cases_date_to_nullable.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeCasesDateToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cases', function (Blueprint $table) {
+            $table->datetime('date', 0)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('nullable', function (Blueprint $table) {
+            //
+        });
+    }
+}
