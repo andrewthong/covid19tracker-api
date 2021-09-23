fe5b2ea0b874f0953a30a29c6ed382d7484f08f6
Andrew <contact@andrewthong.com>
2021-09-22 23:28:29 -0700

    date and stat report test

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -13,9 +13,7 @@ class ReportTest extends TestCase
 {
 
     /**
-     * A basic feature test example.
-     *
-     * @return void
+     * base reports
      */
     public function test_reports()
     {
@@ -38,6 +36,9 @@ class ReportTest extends TestCase
         );
     }
 
+    /**
+     * reports for only one province
+     */
     public function test_province_reports()
     {
         $response = $this->json('GET', '/reports/province/ab?after=2021-08-01');
@@ -52,4 +53,43 @@ class ReportTest extends TestCase
                 ->etc()
         );
     }
+
+    /**
+     * reports for only one province and one date
+     */
+    public function test_province_single_date_report()
+    {
+        $response = $this->json('GET', '/reports/province/ab?date=2021-01-15');
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->where('province', 'ab')
+                ->has('last_updated')
+                ->has('data', 1)
+                ->has('data.0', fn ($json) =>
+                    $json->where('date', '2021-01-15')
+                        ->etc()
+                )
+                ->etc()
+        );
+    }
+
+    /**
+     * reports for one province, one date and one pair of stat
+     * boosters_1 vaccine report check
+     */
+    public function test_province_single_date_stat_report()
+    {
+        $response = $this->json('GET', '/reports/province/ab?date=2021-09-21&stat=boosters_1');
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->where('province', 'ab')
+                ->has('last_updated')
+                ->has('data', 1)
+                ->has('data.0', fn ($json) =>
+                    $json->where('date', '2021-09-21')
+                        ->has('change_boosters_1')
+                        ->has('total_boosters_1')
+                )
+                ->etc()
+        );
+    }
+
 }
