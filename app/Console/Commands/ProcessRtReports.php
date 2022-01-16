41724c3c9d33a7433221d9159f8223745fbc85fe
Andrew <contact@andrewthong.com>
2022-01-16 12:15:02 -0800

    readiness
    - bar advances when going through queue
    - console lines (if nothing to process)
    - last updated timestamp
    - cache clearing

--- app/Console/Commands/ProcessRtReports.php
+++ app/Console/Commands/ProcessRtReports.php
@@ -92,74 +92,81 @@ class ProcessRtReports extends Command
             ->limit($limit)
             ->get();
         
-        $this->line(" >> {$test_results->count()} new test results found");
+        if( $test_results->count() > 0 ) {
 
-        $bar = $this->output->createProgressBar( count($test_results) );
-        $bar->start();
+            $this->line(" >> {$test_results->count()} new test results found");
+            
+            $bar = $this->output->createProgressBar( count($test_results) );
+            $bar->start();
 
-        // loop through test results
-        foreach ($test_results as $test) {
-            $province = null;
-            $date = null;
-            // verify forward sortation area (via first letter)
-            $first_letter = substr($test->postal_code, 0, 1);
-            if( isset($postal_dict[$first_letter]) ) {
-                $province = $postal_dict[$first_letter];
-            }
-            // Nunavut and Northwest Territories share X
-            // defaults to NU but we can detect NT
-            if( $first_letter == 'X' ) {
-                if( in_array($test->postal_code, ['X0E', 'X0G']) ) {
-                    $province = 'NT';
+            // loop through test results
+            foreach ($test_results as $test) {
+                $province = null;
+                $date = null;
+                // verify forward sortation area (via first letter)
+                $first_letter = substr($test->postal_code, 0, 1);
+                if( isset($postal_dict[$first_letter]) ) {
+                    $province = $postal_dict[$first_letter];
                 }
-            }
-            // verify date
-            if( !RapidTest::isTestDateInvalid($test->test_date) ) {
-                $date = $test->test_date;
-            }
-
-            // continue only if province and date are valid
-            if( $province && $date ) {
-
-                // queue key
-                $queue_key = "{$province}_{$date}";
-                
-                // check if queue_key exists
-                if( !isset($queue[$queue_key]) ) {
-                    $queue[$queue_key] = $report_template;
-                    $queue[$queue_key]['province'] = $province;
-                    $queue[$queue_key]['date'] = $date;
+                // Nunavut and Northwest Territories share X
+                // defaults to NU but we can detect NT
+                if( $first_letter == 'X' ) {
+                    if( in_array($test->postal_code, ['X0E', 'X0G']) ) {
+                        $province = 'NT';
+                    }
+                }
+                // verify date
+                if( !RapidTest::isTestDateInvalid($test->test_date) ) {
+                    $date = $test->test_date;
                 }
 
-                // increment appropriate test result
-                $increment_col = $test->test_result;
-                // fallback "invalid result" and other test results
-                if( !in_array($increment_col, ['positive', 'negative']) ) {
-                    $increment_col = 'invalid';
+                // continue only if province and date are valid
+                if( $province && $date ) {
+
+                    // queue key
+                    $queue_key = "{$province}_{$date}";
+                    
+                    // check if queue_key exists
+                    if( !isset($queue[$queue_key]) ) {
+                        $queue[$queue_key] = $report_template;
+                        $queue[$queue_key]['province'] = $province;
+                        $queue[$queue_key]['date'] = $date;
+                    }
+
+                    // increment appropriate test result
+                    $increment_col = $test->test_result;
+                    // fallback "invalid result" and other test results
+                    if( !in_array($increment_col, ['positive', 'negative']) ) {
+                        $increment_col = 'invalid';
+                    }
+                    $queue[$queue_key][$increment_col] += 1;
+
+                    // update processing status
+                    $test->update([$key => 'processed']);
+
+                } else {
+                    // update processing status
+                    $test->update([$key => 'ignored']);
                 }
-                $queue[$queue_key][$increment_col] += 1;
 
-                // update processing status
-                $test->update([$key => 'processed']);
-
-            } else {
-                // update processing status
-                $test->update([$key => 'ignored']);
+                $bar->advance();
             }
 
-            $bar->advance();
+            $bar->finish();
+            $this->line('');
+
+        } else {
+            $this->line(" >> No new test results found");
         }
 
-        $bar->finish();
-
-        $this->line('');
-        $this->line(' >> Processing queue...');
-
-        $bar2 = $this->output->createProgressBar( count($queue) );
-        $bar2->start();
-
         // perform database operations
-        if( $queue ) {
+        if( count($queue) > 0 ) {
+            
+            $this->line(' >> Processing queue...');
+
+            $bar2 = $this->output->createProgressBar( count($queue) );
+            $bar2->start();
+
             foreach( $queue as $queue_item ) {
                 $report = RapidTestReport::updateOrCreate([
                     'province' => $queue_item['province'],
@@ -175,11 +182,25 @@ class ProcessRtReports extends Command
 
                 // save
                 $report->save();
+
+                $bar2->advance();
             }
-            $bar2->advance();
+            $bar2->finish();
+            $this->line('');
         }
 
-        $bar2->finish();
+        // clear cache if needed
+        if( count($queue) > 0 && !$options['noclear'] ) {
+
+            $updated_timestamp = date('Y-m-d H:i:s');
+            Option::set( 'rapid_test_last_processed', $updated_timestamp );
+
+            $this->line(' >> Clearing cache...');
+            Utility::clearCache();
+        }
+
+        // add log entry
+        Utility::log('process_rapid_tests', count($test_results));
 
         $this->line('');
         $this->line(" <fg=green;bg=black>Processing complete.</>");
