24796c1a4494ea237dc513f904fd013b44eb7e6d
Andrew <contact@andrewthong.com>
2022-01-15 22:45:52 -0800

    helpers for processing

--- app/RapidTest.php
+++ app/RapidTest.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Carbon;
+
 // mongoDB
 use Jenssegers\Mongodb\Eloquent\Model;
 
@@ -19,7 +21,17 @@ class RapidTest extends Model
         'test_date',
         'test_result',
         'ip',
+        '_q',
     ];
+
+    const report_status_key = '_q';
+
+    /**
+     * quick helper to return report status key
+     */
+    static function reportStatusKey() {
+        return static::report_status_key;
+    }
 
     /**
      * available values for test result
@@ -30,6 +42,33 @@ class RapidTest extends Model
             'negative',
             'invalid result'
         ];
+    }
+
+    /**
+     * helper function to ensure test dates are in valid range
+     * returns false if valid
+     * returns error message if not valid
+     */
+    static function isTestDateInvalid($test_date) {
+
+        // earliest test date supported
+        $min_date = env('RAPID_TESTS_START_DATE', '2021-12-01');
+        $min_u = strtotime($min_date);
+
+        // latest valid date, using St Johns as reference
+        $max_u = Carbon\Carbon::now('America/St_Johns')->timestamp;
+        $max_date = date('Y-m-d', $max_u);
+
+        $the_date = strtotime($test_date);
+
+        if( $the_date < $min_u ) {
+            return "Test date cannot be before {$min_date}";
+        }
+        if( $the_date >= $max_u ) {
+            return "Test date cannot be after {$max_date}";
+        }
+
+        return false;
     }
 
 }
