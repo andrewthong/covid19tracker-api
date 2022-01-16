710324a1b4955b751b8f90d8762433d57029a532
Andrew <contact@andrewthong.com>
2022-01-15 22:45:40 -0800

    postal district model

---
+++ database/seeders/PostalDistrictSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Flynsarmy\CsvSeeder\CsvSeeder;
+
+use DB;
+
+class PostalDistrictSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $table = 'postal_districts';
+
+        // truncate: comment out if using to add data
+        // DB::table($table)->truncate();
+
+        // use csv to seed database
+        $seed_csv = base_path()."/database/seeders/csv/{$table}.csv";
+        if( file_exists( $seed_csv ) ) {
+            $seeder = new \Flynsarmy\CsvSeeder\CsvSeeder;
+            $seeder->table = $table;
+            $seeder->filename = $seed_csv;
+            $seeder->run();
+        }
+    }
+}
