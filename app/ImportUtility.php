c9e98e055241db08249374b53cba531a8be3bb80
Andrew <contact@andrewthong.com>
2022-06-18 18:37:00 -0700

    switch to diff file processing

--- app/ImportUtility.php
+++ app/ImportUtility.php
@@ -18,12 +18,10 @@ class ImportUtility extends Model
 
     const timestamp_option = 'import_utility_last_timestamp';
 
-    const cases_required_keys = [
+    const required_keys = [
         'region',
         'sub_region_1',
         'date',
-        'value',
-        'value_daily',
     ];
 
     /**
@@ -50,20 +48,6 @@ class ImportUtility extends Model
         $last_import_value = Option::get( self::timestamp_option );
         return $last_import_value !== $current_import_value;
     }
-    
-    /**
-     * helper to get timestamp from source
-     */
-    private static function getTimestamp() {
-        $env_key = "TIMESTAMP_URL";
-        if(self::isEnabled($env_key)) {
-            // get contents of URL
-            $url = self::getEnv($env_key);
-            $value = file_get_contents($url);
-            $value = str_replace(array("\r", "\n"), '', $value);
-            return $value;
-        }
-    }
 
     /**
      * helper to set timestamp
@@ -73,15 +57,45 @@ class ImportUtility extends Model
     }
 
     /**
+     * helper to load designated meta.json
+     */
+    private static function getMeta() {
+        $env_key = "META_FILE_PATH";
+        if(self::isEnabled($env_key)) {
+            // get contents of file
+            $file_path = self::getEnv($env_key);
+            $content = file_get_contents($file_path);
+            # convert json to array
+            $output = json_decode($content, true);
+            return $output;
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * helper to update meta information
+     */
+    private static function updateMeta() {
+       $meta = self::getMeta();
+       self::setTimestamp( $meta['update_time'] ); 
+    }
+
+    /**
      * helper to parse raw csv into array of arrays
      * based off: https://www.php.net/manual/en/function.str-getcsv.php#117692
      */
     private static function parseCsv( $raw_data ) {
         $csv = array_map( 'str_getcsv', $raw_data );
-        array_walk($csv, function(&$a) use ($csv) {
-          $a = array_combine($csv[0], $a);
+        $headers = array_shift($csv);
+        array_walk($csv, function(&$a) use ($headers) {
+            # if length differs, create empty
+            if( count($a) !== count($headers) ) {
+                $a = [];
+            } else {
+                $a = array_combine($headers, $a);
+            }
         });
-        array_shift($csv);
         return $csv;
     }
 
@@ -94,23 +108,36 @@ class ImportUtility extends Model
         return array_diff_key( array_flip( $required ), $row );
     }
 
-    public static function processCases() {
-        $env_key = "CASES_URL";
-        if(!self::isenabled($env_key)) {
-            return false;
+    /**
+     * helper to check if diff file exists
+     */
+    private static function hasDiffFile() {
+        $env_key = "DIFF_FILE_PATH";
+        if(self::isEnabled($env_key)) {
+            $diff_file = self::getEnv($env_key);
+            return file_exists($diff_file);
         }
-        
-        // // timestamp gate
-        // $timestamp = self::getTimestamp();
-        // // compare with saved timestamp
-        // if(!self::hasNewUpdate($timestamp)) {
-        //     return false;
-        // }
+        return false;
+    }
 
-        // retrieve cases
-        $url = self::getEnv($env_key);
+    /**
+     * helper to delete diff file (informs script we are done)
+     */
+    private static function deleteDiffFile() {
+        // delete diff file
+        $diff_file = self::getEnv("DIFF_FILE_PATH");
+        unlink($diff_file);
+    }
+
+    /**
+     * primary function to procss diff file and update database
+     */
+    private static function processDiffFile() {
+
+        // retrieve diff file
+        $diff_file_path = self::getEnv("DIFF_FILE_PATH");
         // expected output is a csv file
-        $data = file($url);
+        $data = file( $diff_file_path );
         $csv = self::parseCsv($data);
 
         $response = [
@@ -120,12 +147,13 @@ class ImportUtility extends Model
         ];
 
         // it's looping time
-        foreach($csv as $row) {
+        foreach( $csv as $row ) {
             // validate row
-            if( count( self::validateRow( self::cases_required_keys, $row ) ) > 0 ) {
+            if( count( self::validateRow( self::required_keys, $row ) ) > 0 ) {
                 continue;
             }
             // check if exists
+            $updated = false;
             $entry = HrReport::firstOrNew([
                 'hr_uid' => $row['sub_region_1'],
                 'date' => $row['date'],
@@ -137,25 +165,60 @@ class ImportUtility extends Model
                     Utility::log( 'ImportUtility', 'Unrecognized HR UID', $row['sub_region_1'], $row['region'] );
                     continue;
                 }
+                $updated = true;
                 $response['created']++;
             }
-            $cases_value = (int)$row['value'];
+            $cases_value = (int)$row['cases'];
+            $fatalities_value = (int)$row['fatalities'];
             // set values; only if differs
             if( $cases_value !== $entry->cases ) {
-                $entry->cases = $row['value'];
-                // count updated only if exists
-                if ($entry->exists) {
-                    $response['updated']++;
-                }
+                $entry->cases = $cases_value;
+                $updated = true;
+            }
+            if( $fatalities_value !== $entry->fatalities ) {
+                $entry->fatalities = $fatalities_value;
+                $updated = true;
+            }
+            // count updated only if exists
+            if ($updated && $entry->exists) {
+                $response['updated']++;
+            }
+            if ($updated) {
                 $entry->save();
             }
         }
 
-        // save timestamp
-        self::setTimestamp( $timestamp );
-
+        Utility::log( 'ImportUtility', 'Diff processed', $response );
         return $response;
         
+    }
+
+    /**
+     * process
+     */
+    public static function process() {
+
+        if(!self::isenabled()) {
+            return false;
+        }
+
+        // check if diff file exists
+        if( self::hasDiffFile() ) {
+
+            // process diff file
+            $result = self::processDiffFile();
+
+            // load and update meta information
+            self::updateMeta();
+
+            // wrap up
+            self::deleteDiffFile();
+
+            return $result;
+
+        }
+
+        return "No diff file found";
     }
 
 }
